ce7ce185727b202449170eac11e89eb533abc184
George Aidonidis <george.aidonidis@gmail.com>
2018-10-16 20:29:07 +0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const { router, get, post, options } = require('microrouter');
+const {router, get, post, options} = require('microrouter');
+const cors = require('micro-cors')();
 const graphqlHandler = require('./functions/graphql');
 const health = require('./functions/health');
 
+const handler = cors(graphqlHandler);
 module.exports = router(
 	get('/', health),
-	options('/graphql', graphqlHandler),
-	post('/graphql', graphqlHandler),
-	get('/graphql', graphqlHandler),
+	options('/graphql', handler),
+	post('/graphql', handler),
+	get('/graphql', handler)
 );
